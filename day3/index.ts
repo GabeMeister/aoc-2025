c902ccb1730163c676e83d23db02063da88d1acc
Gabe Jensen <thegabejensen@gmail.com>
2025-12-23 12:08:15 -0800

    Day 3 pt 1 complete, need to submit pt2

---
+++ day3/index.ts
@@ -0,0 +1,94 @@
+// const input = await Bun.file("input_shortest.txt").text();
+const input = await Bun.file("input_short.txt").text();
+// const input = await Bun.file("input_long.txt").text();
+const banks = input.trim().split("\n");
+if (!banks) {
+  process.exit(0);
+}
+
+const getHighestJoltagePt1 = (bank: string): number => {
+  // Iterate to find largest first digit
+  const firstDigit = Array.from(bank.slice(0, bank.length - 1)).reduce(
+    (highest, curr, idx) => {
+      const currDigit = parseInt(curr);
+      if (currDigit > highest.digit) {
+        return { digit: currDigit, idx };
+      } else {
+        return highest;
+      }
+    },
+    { digit: 0, idx: 0 }
+  );
+
+  // Iterage to find largest second digit
+  const secondDigit = Array.from(
+    bank.slice(firstDigit.idx + 1, bank.length)
+  ).reduce(
+    (highest, curr, idx) => {
+      const currDigit = parseInt(curr);
+      if (currDigit > highest.digit) {
+        return { digit: currDigit, idx };
+      } else {
+        return highest;
+      }
+    },
+    { digit: 0, idx: 0 }
+  );
+
+  const result = parseInt(`${firstDigit.digit}${secondDigit.digit}`);
+
+  return result;
+};
+
+const part1 = () => {
+  const total = banks.reduce((acc, bank) => {
+    return acc + getHighestJoltagePt1(bank);
+  }, 0);
+
+  console.log("Part 1:", total);
+};
+
+const getHighestJoltagePt2 = (bank: string) => {
+  // There's up to 3 "free spaces" that you can play around with when trying to
+  // find the maximum 12 digit number
+  let freeSpaces = 3;
+  let startingIdx = 0;
+  let result = "";
+
+  while (freeSpaces > 0) {
+    let tmpHighestIdx = 0;
+    let tmpHighest = 0;
+
+    // Find the potential highest digit with the amount of free spaces we have
+    // left
+    for (let i = startingIdx; i <= startingIdx + freeSpaces; i++) {
+      const digit = parseInt(bank[i] ?? "0");
+      if (digit > tmpHighest) {
+        tmpHighest = digit;
+        tmpHighestIdx = i;
+      }
+    }
+
+    freeSpaces -= tmpHighestIdx;
+    startingIdx = tmpHighestIdx + 1;
+
+    result += bank[tmpHighestIdx] ?? "";
+  }
+
+  // Always need to add back 9 spaces
+  const extras = bank.slice(startingIdx, startingIdx + 9);
+  result += extras;
+
+  return parseInt(result);
+};
+
+const part2 = () => {
+  const total = banks.reduce((acc, bank) => {
+    return acc + getHighestJoltagePt2(bank);
+  }, 0);
+
+  console.log("Part 2:", total);
+};
+
+part1();
+part2();
